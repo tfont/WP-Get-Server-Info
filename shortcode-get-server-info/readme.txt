458809ef399ed8e1e35bcb8643bde51936ccc608
Travis van der Font <travis.font@gmail.com>
2017-06-27 15:15:27 +0200

    -

---
+++ shortcode-get-server-info/readme.txt
@@ -0,0 +1,29 @@
+=== All Annuaires Shortcode ===
+Tags:              php, server, post, get, shortcode
+Requires at least: 1.0
+Tested up to:      1.0
+Stable tag:        1.0
+License:           MIT
+License URI:       http://www.travisfont.com
+
+Retrieves $_SEVER, $_POST, or $_GET. Rquires PHP 5.6+.
+
+== Description ==
+Retrieves $_SEVER, $_POST, or $_GET. Rquires PHP 5.6+.
+
+
+Will display GET values with "print_r"
+[get-server-info=get type=print_r]
+
+Will display POST values with "<pre>" HTML element wrapped around "print_r"
+[get-server-info=post type=pre_print_r]
+
+Will display SERVER values with "var_dump"
+[get-server-info=server type=var_dump]
+
+Will use the default value which is "print_r" and display GET values
+[get-server-info=get]
+
+
+== Installation ==
+Activate and use shortcode: [get-server-info="post"]
---
+++ shortcode-get-server-info/shortcode-get-server-info.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+Plugin Name: Shortcode - Gets Server Info (get, post, server)
+Plugin URI:  http://www.travisfont.com
+Description: Shortcode - Gets Server Info (get, post, server)
+Version:     1.0.0
+Author URI:  http://www.travisfont.com
+*/
+
+namespace
+{
+    // don't load directly
+    defined('ABSPATH') || exit;
+    defined('WPINC')   || exit;
+
+    if (!class_exists('GetServerInfoShortcode'))
+    {
+        abstract class GetServerInfoShortcodeParameters
+        {
+            // Defined Shortcode Tag
+            const SHORTCODE = 'get-server-info';
+
+            // Available Shortcode Attributes (defaults)
+            const PARAMETERS = array
+            (
+                'ids'   => NULL,
+                'class' => NULL,
+                'col'   => 'col-md-2'
+            );
+
+            const POST_TYPE = NULL;
+        }
+
+        final class GetServerInfoShortcode extends \GetServerInfoShortcodeParameters
+        {
+            public function __construct()
+            {
+                add_action('init', array($this, 'init'), 1);
+            }
+
+            public static function init()
+            {
+                // Apply the shortcode
+                add_shortcode(self::SHORTCODE, function ($attributes)
+                {
+                    return self::output($attributes);
+                });
+            }
+
+            private static function output($attributes = NULL)
+            {
+                // Return output data (HTML text within a span)
+                switch (preg_replace('/[^A-Z]+/', '', strtoupper(trim($attributes[0]))))
+                {
+                    case 'SERVER':
+                        $data = $_SERVER;
+                        break;
+                    case 'GET':
+                        $data = $_GET;
+                        break;
+                    case 'POST':
+                        $data = $_POST;
+                        break;
+                }
+
+                // SERVER, GET, or POST exist
+                if (isset($data))
+                {
+                    // if type parameter exist
+                    if (isset($attributes['type']) && !empty($attributes['type']))
+                    {
+                        // clean format the type parameter + look for matching value
+                        switch (preg_replace('/[^A-Z_]+/', '', strtoupper(trim($attributes['type']))))
+                        {
+                            case 'VAR_DUMP':
+
+                                var_dump($data);
+
+                                break;
+
+                            case 'PRE_PRINT_R':
+
+                                echo '<pre>';
+                                print_r($data);
+                                echo '</pre>';
+
+                                break;
+
+                            case 'PRINT_R':
+                            default:
+
+                                print_r($data);
+                        }
+                    }
+                    else
+                    {
+                        // no type parameter exist - default print_r is used
+                        print_r($data);
+                    }
+                }
+            }
+        }
+    }
+
+    (new \GetServerInfoShortcode);
+}
